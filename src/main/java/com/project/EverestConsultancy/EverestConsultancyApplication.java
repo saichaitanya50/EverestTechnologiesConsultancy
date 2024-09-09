e9aca1a52c8ce954760539c421489dd472db78fa
Sai chaitanya Dhanisetty <chatni.chatni50@gmail.com>
2024-09-09 15:40:25 -0400

    Updated pom.xml,Added swagger Ui details in  EverestConsultancyApplication.java, and application.properties

--- src/main/java/com/project/EverestConsultancy/EverestConsultancyApplication.java
+++ src/main/java/com/project/EverestConsultancy/EverestConsultancyApplication.java
@@ -3,8 +3,24 @@ package com.project.EverestConsultancy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Welcome to Everest Technologies",
+				version = "1.0.0",
+				description = "This is the Documentation for Everest Student Consultancy Rest API",
+				termsOfService = "by chaitnaya",
+				contact  = @Contact(
+						name = "Sai Chaitanya",
+						email = "saichaitanya9989@gmail.com"),
+				license = @License(
+						name = "license")))
 public class EverestConsultancyApplication {
 
 	public static void main(String[] args) {
